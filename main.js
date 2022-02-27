a1b957958eaae99b4524837ed87747f20550c31c
King <pic.ner101@gmail.com>
2022-02-27 13:06:08 +0000

    3 apis to get weather utilizing ip

--- main.js
+++ main.js
@@ -1,10 +1,58 @@
-fetch('https://api.ipify.org?format=json', {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
-    .then(res => res.json())
-    .then(res => {
-        console.log(res)
+import axios from 'axios'
+import apikey from './apiKey'
+
+
+async function weatherByIp() {
+    let ip = ""
+    let latlong = ''
+
+    // fetch the IP
+    await axios({
+        method: 'GET',
+        url: 'https://api.ipify.org?format=json',
+        headers: {
+            // 'Content-Type': 'application/json',
+        },
     })
+        .then(res => {
+
+            ip = res.data.ip
+            console.log(ip)
+        })
+
+
+    // fetch latlong from ip 
+    await axios({
+        // method: 'GET',
+        method: "GET",
+        url: `https://ipapi.co/${ip}/latlong/`,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then(res => {
+        latlong = res.data
+    })
+
+    console.log(latlong)
+
+
+    // fetch weather using latlong
+    // https://www.weatherapi.com/docs/#
+    await axios({
+        method: 'GET',
+        url: "http://api.weatherapi.com/v1/current.json",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        params: {
+            key: apikey,
+            q: latlong
+        }
+    }).then(res => {
+        console.log(res.data)
+    })
+
+
+}
+
+weatherByIp()
